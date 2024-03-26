cd2c0b2fff6106541a2b7063798f31194a8f1677
toda5603 <todat@acm.org>
2024-03-26 23:53:47 +0900

    update

--- test_david.py
+++ test_david.py
@@ -1,28 +1,32 @@
 
-from explainer  import Explainer
 from pydavid    import OpenDavid
 from dhgraph    import DirectedHypergraph
 
-explainer = Explainer()
+def _make_david_problem(observation_list, required_atom_list):
+    if len(observation_list) == 0:
+        raise Exception("no observation")
+    res = ""
+    res += "problem {\n"
+    res += "    observe { "\
+        + " ^ ".join(observation_list) + " }\n"
+    if len(required_atom_list) > 0:
+        res += "    require { "\
+            + " ^ ".join(required_atom_list) + " }\n"
+    res += "}\n"
+    return res
 
 OpenDavid.set_david_path("/usr/local/bin/david")
-with open("kb.dav", mode="r") as f:
+with open("data/kb.dav", mode="r") as f:
     data = f.read()
     OpenDavid.set_knowledge_base(data)
-
 observation_list = ["say_seer(4, 4)", "vote(3,2)", "say_seer(1,1)",\
-                    "vote(5,4)", "say_divined_human(4,1,4)",\
-                    "say_divined_werewolf(1,4,1)", "vote(4,3)",\
-                    "vote(1,4)", "say_seer(3,3)", "say_divined_human(3,2,3)",\
+                    "vote(5,4)", "say_divined_human(4,1)",\
+                    "say_divined_werewolf(1,4)", "vote(4,3)",\
+                    "vote(1,4)", "say_seer(3,3)", "say_divined_human(3,2)",\
                     "say_villager(2,2)"]
-for atom_str in observation_list:
-    explainer.add(atom_str)
-
-#sentence = explainer.explain_possibility("seer(3)")
-
-data = explainer._make_david_problem([atom_str])
+data = _make_david_problem(observation_list, ["seer(3)"])
 OpenDavid.set_problem(data)
 json_str = OpenDavid.run()
-graph = OpenDavid.build_proofgraph(json_str)
-graph.render()
+g = OpenDavid.build_proofgraph(json_str)
+g.render(filename="sample", format="png")
 
